b444155b92da3bd3dc839c89a79f3d763712a7f1
Immortal-Dream <tonyliu02@outlook.com>
2025-02-17 04:00:39 +0000

    updates from stencil

--- distribution/all/mem.js
+++ distribution/all/mem.js
@@ -5,7 +5,7 @@ const util = distribution.util;
 function mem(config) {
   const context = {};
   context.gid = config.gid || 'all';
-  context.hash = config.hash || util.id.naiveHash;
+  context.hash = config.hash || global.distribution.util.id.naiveHash;
 
   /* For the distributed mem service, the configuration will
           always be a string */
